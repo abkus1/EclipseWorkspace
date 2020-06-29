4ae6f728ee7b5bd46de89c7a4de68a4ad18dcc6c
abkus1 <abhaykushwaha@hotmail.com>
2020-06-29 19:41:40 +0530

    uploading new classes

---
+++ JavaTraining/src/ListConcept/LinkedListConcept.java
@@ -0,0 +1,20 @@
+package ListConcept;
+
+import java.util.LinkedList;
+public class LinkedListConcept {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> ll=new LinkedList<String>();
+		
+		ll.add("test");
+		ll.add("qtp");
+		ll.add("selenium");
+		ll.add("RPA");
+		ll.add("RFT");
+		
+		//print
+		System.out.println("Contents of linkedlist: "+ ll);
+	}
+
+}
